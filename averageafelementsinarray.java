92a96393ef4778f996cb2523aec4f662285e008e
Priyanshu Raj <xhptpriyanshu@gmail.com>
2026-09-13 13:05:11 +0530

    Add files via upload
    A program to find the average of elements in an array.

---
+++ averageafelementsinarray.java
@@ -0,0 +1,25 @@
+package priiiii;
+import java.util.Scanner;
+public class addition {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number of no arrays you want to store");
+int n=sc.nextInt();
+
+int []a=new int[n];
+System.out.println("enter the values to store");
+int i;
+for(i=0;i<n;i++) {
+	a[i]=sc.nextInt();
+}int sum=0;for(i=0;i<n;i++) {
+	System.out.println("The value in the Sttring are :"+a[i]);
+	if(a[i]>=0) {
+		sum=a[i]+sum;
+	}
+}int average=sum/n;
+System.out.println("The average of the elements of array :"+average);
+
+}}
