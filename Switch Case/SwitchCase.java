1c489c2d9db48782980610da9a1aa41b8d4efae5
Priyanshu Raj <xhptpriyanshu@gmail.com>
2026-09-15 17:18:39 +0530

    A menu driven program on Switch case

--- Switch Case/SwitchCase.java
+++ Switch Case/SwitchCase.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class SwitchCase {
     public static void main(String[] args) {
       System.out.println("Hello, World!");
 Scanner sc=new Scanner(System.in);
